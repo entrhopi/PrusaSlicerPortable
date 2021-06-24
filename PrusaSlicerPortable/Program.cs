6653c119d2466c5ac4a662fe34dc74799ab3f149
EntRhoPi <alexander@freynik.org>
2021-06-24 10:41:04 +0200

    code for running external application

--- PrusaSlicerPortable/Program.cs
+++ PrusaSlicerPortable/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,11 @@ namespace PrusaSlicerPortable
     {
         static void Main(string[] args)
         {
+            Process ExternalProcess = new Process();
+            ExternalProcess.StartInfo.FileName = "cmd.exe";
+            ExternalProcess.StartInfo.Arguments = "/c start prusa-slicer.exe --datadir profile";
+            ExternalProcess.StartInfo.UseShellExecute = false;
+            ExternalProcess.Start();
         }
     }
 }
